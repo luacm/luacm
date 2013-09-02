fe2e5c8f1f0f684b0c6892853b247f1084a692e0
Greyson Parrelli <greyson.parrelli@live.com>
2013-09-02 12:51:44 -0400

    Basic output news events working.

---
+++ database.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+exports.newsSchema = mongoose.Schema({
+    title: String,
+    date: Date,
+    body: String, 
+    author: String,
+    tags: [String]
+});
+
+exports.News = mongoose.model('News', exports.newsSchema, 'news');
+
+exports.connect = function() {
+    mongoose.connect(process.env.MONGO_URI);
+    return mongoose.connection;
+}
--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,19 @@
+var mongoose = require('mongoose');
+var database = require('../database');
+
 exports.get = function(req, res) {
-    var data = {
-        title: 'News'
-    }
-    res.render('news', data);
+    var db = database.connect();
+    db.on('error', function() { res.send({'error': 'Failed to connect to the database.'})});
+    db.once('open', function() {
+        database.News.find(function(err, results) {
+            console.log(results);
+            var data = {
+                title: 'News',
+                news: results
+            }
+
+            res.render('news', data);
+        });
+        
+    });
 };
