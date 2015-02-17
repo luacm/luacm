7cb070aa01776a17af79d39c749c8f53c76e5a2e
Greyson Parrelli <greyson.parrelli@gmail.com>
2015-02-16 20:28:02 -0800

    Fixed merge error.

--- partials/mobilehigh_nav_bar.php
+++ partials/mobilehigh_nav_bar.php
@@ -5,10 +5,10 @@
                 <li class="active">
                     <a class="brand" href="index.php">mobiLEHIGH</a>
                 </li>
-                <li><a href="rules.html">Rules</a></li>
-                <li><a href="dates.html">Dates</a></li>
-                <li><a href="getting_started.html">Getting Started</a></li>
-                <li><a href="assets.html">Free Assets</a></li>
+                <li><a href="rules.php">Rules</a></li>
+                <li><a href="dates.php">Dates</a></li>
+                <li><a href="getting_started.php">Getting Started</a></li>
+                <li><a href="assets.php">Free Assets</a></li>
                 <li><a href="http://goo.gl/ITyvMR" target="parent">Sign Up</a></li>
                 <li><a href="http://goo.gl/KXEd5P" target="parent">Find a Team</a></li>
             </ul>
