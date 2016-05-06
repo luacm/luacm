7adfaa737879b9bc148b2c1124c992a87f9ba093
aknuck <arknw229@gmail.com>
2016-05-06 17:28:15 -0400

    got rid of partials

--- partials/imports.php
+++
@@ -1,6 +0,0 @@
-<link href="css/bootstrap.css" rel="stylesheet" />
-<link href="css/bootswatch.min.css" rel="stylesheet" />
-<link href="css/style.css" rel="stylesheet" />
-<script src="js/jquery.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/all.js" type="text/javascript"></script>
--- partials/mobilehigh_imports.php
+++
@@ -1,5 +0,0 @@
-<link href="../mobilehigh/css/bootstrap.superhero.mod.css" rel="stylesheet" />
-<link href="../mobilehigh/css/style.css" rel="stylesheet" />
-
-<script src="../mobilehigh/js/jquery.min.js" type="text/javascript"></script>
-<script src="../mobilehigh/js/bootstrap.min.js" type="text/javascript"></script>
